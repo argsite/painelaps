85c4c9e33a3fd57abe5acd74ac8d169ad4d1ba82
Miro Teixeira <78441204+argsite@users.noreply.github.com>
2026-07-20 08:01:28 -0300

    Update page title in Streamlit configuration

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import plotly.express as px
 import streamlit as st
 
 st.set_page_config(
-    page_title="Saúde 360 APS",
+    page_title="APS 360 - Painel de Indicadores",
     page_icon="📊",
     layout="wide",
     initial_sidebar_state="expanded",
